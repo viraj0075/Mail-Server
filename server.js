c58e0f3759e9a9d44298e1b24b5a86299519d5c8
viraj0075 <viraj.koradia.learn.7979@gmail.com>
2024-10-12 22:23:52 +0530

    minor

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -27,10 +28,9 @@ transporter.verify((error) => {
   }
 });
 
-app.get("/",(req,res) =>
-{
-    res.send("hello")
-})
+app.use("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
 
 app.post('/api/send-email', (req, res) => {
   const { name, email, message } = req.body;
@@ -52,6 +52,10 @@ app.post('/api/send-email', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
